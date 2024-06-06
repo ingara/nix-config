673a8efdefaf6da53de31985bf03c03f9ab6ca02
Ingar Mathisen Almklov <ingara@gmail.com>
2024-06-06 19:31:31 +0200

    Remove nvim-tmux-navigator

--- modules/shared/configs/nvim/lua/plugins/nvim-tmux-navigator.lua
+++ modules/shared/configs/nvim/lua/plugins/nvim-tmux-navigator.lua
@@ -1,23 +1,23 @@
 return {
-  "alexghergh/nvim-tmux-navigation",
-  enabled = enabled,
-  event = "VeryLazy",
-  vscode = false,
-  config = function()
-    local nvim_tmux_nav = require("nvim-tmux-navigation")
-    nvim_tmux_nav.setup({
-      disable_when_zoomed = true,
-      -- defaults to false
-      keybindings = {
-        left = "<C-h>",
-        down = "<C-j>",
-        up = "<C-k>",
-        right = "<C-l>",
-        last_active = "<C-\\>",
-        next = "<C-Space>",
-      },
-    })
-  end,
+  -- "alexghergh/nvim-tmux-navigation",
+  -- enabled = enabled,
+  -- event = "VeryLazy",
+  -- vscode = false,
+  -- config = function()
+  --   local nvim_tmux_nav = require("nvim-tmux-navigation")
+  --   nvim_tmux_nav.setup({
+  --     disable_when_zoomed = true,
+  --     -- defaults to false
+  --     keybindings = {
+  --       left = "<C-h>",
+  --       down = "<C-j>",
+  --       up = "<C-k>",
+  --       right = "<C-l>",
+  --       last_active = "<C-\\>",
+  --       next = "<C-Space>",
+  --     },
+  --   })
+  -- end,
   -- keys = {
   --   { "<C-h>", "<cmd>NvimTmuxNavigateLeft<cr>", { desc = "Tmux navigate left", silent = true } },
   --   { "<C-j>", "<cmd>NvimTmuxNavigateDown<cr>", { desc = "Tmux navigate down", silent = true } },
