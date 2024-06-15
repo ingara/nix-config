71921764a1eb800a9d08c353be1d70b72ab64cb7
Ingar Mathisen Almklov <ingara@gmail.com>
2024-06-15 19:44:03 +0200

    Vim: <C-g> for lazygit

--- modules/shared/configs/nvim/lua/config/keymaps.lua
+++ modules/shared/configs/nvim/lua/config/keymaps.lua
@@ -3,8 +3,11 @@
 -- Add any additional keymaps here
 local map = vim.keymap.set
 
-
-map('', '<leader>y', '"+y', { desc = "Copy to clipboard in normal, visual, select and operator modes" })
-map('n', 'H', '_', { desc = "Start of line"})
-map('n', 'L', '$', { desc = "End of line"})
-map('n', '<leader>uz', ':ZenMode<CR>', { desc = "Toggle Zen mode" })
+map("", "<leader>y", '"+y', { desc = "Copy to clipboard in normal, visual, select and operator modes" })
+map("n", "H", "_", { desc = "Start of line" })
+map("n", "L", "$", { desc = "End of line" })
+map("n", "<leader>uz", ":ZenMode<CR>", { desc = "Toggle Zen mode" })
+-- lazygit
+map("n", "<C-g>", function()
+  LazyVim.lazygit({ cwd = LazyVim.root.git() })
+end, { desc = "Lazygit (Root Dir)" })
