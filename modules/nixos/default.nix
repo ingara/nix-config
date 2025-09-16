a2ba6524075c23a96093ebce422e436d3fdd59d8
Ingar Mathisen Almklov <ingara@gmail.com>
2025-09-16 20:41:06 +0200

    Update configuration files

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,7 +1,21 @@
 { config, lib, pkgs, userConfig, ... }:
 
 {
-  imports = [
-    ./1password.nix
+  # Allow 1Password unfree packages
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "1password-gui"
+    "1password"
   ];
+
+  # Enable polkit for 1Password authentication
+  security.polkit.enable = true;
+
+  # Enable 1Password CLI
+  programs._1password.enable = true;
+  
+  # Enable 1Password GUI (Linux only - macOS uses Homebrew)
+  programs._1password-gui = lib.mkIf pkgs.stdenv.isLinux {
+    enable = true;
+    polkitPolicyOwners = [ userConfig.username ];
+  };
 }
