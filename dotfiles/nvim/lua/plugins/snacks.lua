2b91c79e8e46cf58aef707917b5eae62c255facb
Ingar Mathisen Almklov <ingara@gmail.com>
2025-10-01 08:59:14 +0200

    feat(nvim): open default snacks terminal as float

--- dotfiles/nvim/lua/plugins/snacks.lua
+++ dotfiles/nvim/lua/plugins/snacks.lua
@@ -1,6 +1,14 @@
 return {
   "folke/snacks.nvim",
   opts = {
+    terminal = {
+      win = {
+        position = "float",
+        width = 0.8,
+        height = 0.8,
+        border = "rounded",
+      },
+    },
     picker = {
       sources = {
         files = {
@@ -15,4 +23,3 @@ return {
     },
   },
 }
-
