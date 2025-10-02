b66121e442bec568db0d63ba6a19f50401bbf346
Ingar Mathisen Almklov <ingara@gmail.com>
2025-10-02 09:06:56 +0200

    refactor(nvim): simplify blink.cmp configuration

--- dotfiles/nvim/lua/plugins/blink.lua
+++ dotfiles/nvim/lua/plugins/blink.lua
@@ -1,34 +1,10 @@
 return {
   "saghen/blink.cmp",
-
-  -- dependencies = {
-  --   "Kaiser-Yang/blink-cmp-avante",
-  -- },
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
-    fuzzy = {
-      -- Use Lua implementation as workaround for path resolution bug
-      -- https://github.com/Saghen/blink.cmp/discussions/1341
-      implementation = "lua",
-    },
     sources = {
-      -- adding any nvim-cmp sources here will enable them
-      -- with blink.compat
-      -- compat = {},
       default = { "lsp", "path", "buffer" },
-      -- cmdline = {},
-      -- providers = {
-      --   avante = {
-      --     name = "avante",
-      --     module = "blink-cmp-avante",
-      --     kind = "Avante",
-      --     async = true,
-      --     opts = {
-      --       -- options for blink-cmp-avante
-      --     },
-      --   },
-      -- },
     },
     completion = {
       accept = { auto_brackets = { enabled = false } },
@@ -36,16 +12,16 @@ return {
         border = "rounded",
       },
       documentation = { window = { border = "rounded" } },
+      ghost_text = { enabled = false },
     },
     signature = {
       window = { border = "rounded" },
     },
     keymap = {
       preset = "super-tab",
-      -- https://github.com/LazyVim/LazyVim/issues/61852
       ["<Tab>"] = {
         require("blink.cmp.keymap.presets").get("super-tab")["<Tab>"][1],
-        require("lazyvim.util.cmp").map({ "snippet_forward", "ai_accept" }),
+        require("lazyvim.util.cmp").map({ "snippet_forward", "ai_nes", "ai_accept" }),
         "fallback",
       },
     },
