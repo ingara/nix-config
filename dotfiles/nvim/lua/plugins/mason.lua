84f029c1bb1c26dafc382869ed25631fac9727f4
Ingar Mathisen Almklov <ingara@gmail.com>
2025-10-02 09:07:01 +0200

    feat(nvim): add prettier fallback for biome formatting

--- dotfiles/nvim/lua/plugins/mason.lua
+++ dotfiles/nvim/lua/plugins/mason.lua
@@ -1,63 +1,15 @@
-local function biome_lsp_or_prettier(bufnr)
-  local has_biome_lsp = vim.lsp.get_clients({
-    bufnr = bufnr,
-    name = "biome",
-  })[1]
-  if has_biome_lsp then
-    return {}
-  end
-  local has_prettier = vim.fs.find({
-    -- https://prettier.io/docs/en/configuration.html
-    ".prettierrc",
-    ".prettierrc.json",
-    ".prettierrc.yml",
-    ".prettierrc.yaml",
-    ".prettierrc.json5",
-    ".prettierrc.js",
-    ".prettierrc.cjs",
-    ".prettierrc.toml",
-    "prettier.config.js",
-    "prettier.config.cjs",
-  }, { upward = true })[1]
-  if has_prettier then
-    return { "prettier" }
-  end
-  return { "biome" }
-end
-
 return {
-  -- Use Biome instead of prettier / eslint
+  -- Override LazyVim's biome config to add prettier fallback
   {
     "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        ["javascript"] = biome_lsp_or_prettier,
-        ["javascriptreact"] = biome_lsp_or_prettier,
-        ["typescript"] = biome_lsp_or_prettier,
-        ["typescriptreact"] = biome_lsp_or_prettier,
-        ["vue"] = { "biome" },
-        ["css"] = { "biome" },
-        ["scss"] = { "biome" },
-        ["less"] = { "biome" },
-        ["html"] = { "biome" },
-        ["json"] = { "biome" },
-        ["jsonc"] = { "biome" },
-        ["yaml"] = { "biome" },
-        ["markdown"] = { "biome" },
-        ["markdown.mdx"] = { "biome" },
-        ["graphql"] = { "biome" },
-        ["handlebars"] = { "biome" },
-      },
-    },
-  },
-  {
-    "mason-org/mason.nvim",
-    dependencies = { "mason-org/mason-lspconfig.nvim" },
-    opts = {
-      ensure_installed = {
-        "@biomejs/biome",
-        "nixpkgs-fmt",
-      },
-    },
+    opts = function(_, opts)
+      opts.formatters = opts.formatters or {}
+      opts.formatters.prettier = { require_cwd = true }
+
+      -- Add prettier as fallback for all biome-supported filetypes
+      for _, ft in ipairs({ "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "css", "json", "jsonc" }) do
+        opts.formatters_by_ft[ft] = { "biome", "prettier", stop_after_first = true }
+      end
+    end,
   },
 }
