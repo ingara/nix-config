740c274713347f68b762aff69fb020dc6f34184b
Ingar Mathisen Almklov <ingara@gmail.com>
2025-10-02 09:14:27 +0200

    feat(nvim): configure nixd LSP server

--- dotfiles/nvim/lua/plugins/nvim-lspconfig.lua
+++ dotfiles/nvim/lua/plugins/nvim-lspconfig.lua
@@ -5,6 +5,11 @@ return {
     ---@type lspconfig.options
     servers = {
       biome = {},
+      -- Use nixd instead of nil_ls (LazyVim's nix extra default)
+      nixd = {},
+      nil_ls = {
+        mason = false, -- Disable Mason auto-install, use system nixd instead
+      },
     },
     inlay_hints = { enabled = false },
   },
